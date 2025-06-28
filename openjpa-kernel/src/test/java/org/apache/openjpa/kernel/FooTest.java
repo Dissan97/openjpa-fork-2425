a0b3fe500e4b436b9acf69be43b6a44f9a97b6b0
dissan-ahmed <dissanahmed@gmail.com>
2025-06-28 19:30:53 +0200

    pom update and CI&CT added

--- openjpa-kernel/src/test/java/org/apache/openjpa/kernel/FooTest.java
+++ openjpa-kernel/src/test/java/org/apache/openjpa/kernel/FooTest.java
@@ -1,5 +1,9 @@
 package org.apache.openjpa.kernel;
 
+import org.apache.openjpa.abstractstore.AbstractStoreBrokerFactory;
+import org.apache.openjpa.conf.OpenJPAConfiguration;
+import org.apache.openjpa.conf.OpenJPAConfigurationImpl;
+import org.apache.openjpa.datacache.DataCacheStoreManager;
 import org.apache.openjpa.util.ProxyManagerImpl;
 import org.junit.Test;
 
@@ -7,16 +11,13 @@ import java.util.HashMap;
 import java.util.Map;
 
 import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
 
 public class FooTest {
 
     @Test
     public void testFoo() {
-        Map<String, String> props = new HashMap<>();
-        props.put("foo", "bar");
-        props.put("bar", "baz");
-        ProxyManagerImpl pm = new ProxyManagerImpl();
-        pm.copyCustom(props);
-        assertTrue(true);
+
+
     }
 }
